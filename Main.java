78ea437e2c2635559b20e86a9f3226ae4c5ccd3c
imshari29 <160562050+imshari29@users.noreply.github.com>
2024-11-30 20:02:35 +0530

    Add files via upload
    java basics

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        String s = in.next();
+        String c = in.next();
+        System.out.println(num);
+        System.out.println(s);
+        System.out.println(c);
+    }
+}
