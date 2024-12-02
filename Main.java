078caf8e7f6c9c3a1cd95e2c94c22d218f1a0b8e
imshari29 <160562050+imshari29@users.noreply.github.com>
2024-12-02 20:25:13 +0530

    Add files via upload
    Patterns

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-
 class Main{
-    public static void main (String[] args){
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        String s = in.next();
-        String c = in.next();
-        System.out.println(num);
-        System.out.println(s);
-        System.out.println(c);
+    public static void main(String[] args) {
+       int n = 5;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
     }
 }
